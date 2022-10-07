86aa92b0bdb7741b72078f09ebeab8e5a5560e92
Gagan Kaushik <gagan.kaushik@nagarro.com>
2022-10-07 17:55:33 +0530

    adding files

---
+++ test/test/src/test/java/Com/Sample/testcases/LoginTest.java
@@ -0,0 +1,70 @@
+package Com.Sample.testcases;
+
+import java.io.IOException;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import Com.Sample.base.basepage;
+import Com.Sample.pages.LoginPage;
+import Com.Sample.utilities.PropertiesFile;
+import Com.Sample.utilities.ReadExcelData;
+import Com.Sample.utilities.testdataexecution;
+
+public class LoginTest extends basepage {
+	LoginPage lp;
+	private static Logger logger = LogManager.getLogger(LoginTest.class);
+	public LoginTest() {
+		super();
+	}
+
+	@BeforeMethod
+	public void Setup() throws IOException {
+		setup();
+		lp = new LoginPage(null);
+	}
+
+	@Test(priority = 1, groups= {"smoke"})
+	public void correctemailtest() throws Exception {
+
+		driver.findElement(LoginPage.btn_Login1).click();
+		Thread.sleep(2000);
+		driver.findElement(LoginPage.btn_Login2).click();
+		//driver.findElement(LoginPage.txt_username).sendKeys(PropertiesFile.prop.getProperty("number"));
+		//driver.findElement(LoginPage.txt_password).sendKeys(PropertiesFile.prop.getProperty("password"));
+		
+		ReadExcelData excel = new ReadExcelData();
+		String EXECUTION_REQUIRED = excel.readingData(2, 1);
+        testdataexecution.toCheckExecutionRequired(EXECUTION_REQUIRED);
+        String USERID = excel.readingData(0, 1);
+        
+        String PASSWORD = excel.readingData(1, 1);
+        driver.findElement(LoginPage.txt_username).sendKeys(USERID);
+        driver.findElement(LoginPage.txt_password).sendKeys(PASSWORD);
+		driver.findElement(LoginPage.btn_button1).click();
+		Thread.sleep(3000);
+		Assert.assertEquals(driver.getTitle(),
+				"Online Shopping Site for Mobiles, Electronics, Furniture, Grocery, Lifestyle, Books & More. Best Offers!");
+		System.out.println("Assertion is passed");		
+	}
+
+	@Test(priority = 2, groups= {"smoke"})
+	public void Incorrectemailtest() throws InterruptedException {
+
+		driver.findElement(LoginPage.btn_Login1).click();
+		Thread.sleep(2000);
+		driver.findElement(LoginPage.btn_Login2).click();
+		driver.findElement(LoginPage.txt_username).sendKeys(PropertiesFile.prop.getProperty("number"));
+		driver.findElement(LoginPage.txt_password).sendKeys(PropertiesFile.prop.getProperty("password2"));
+		driver.findElement(LoginPage.btn_button1).click();
+		Thread.sleep(3000);
+		driver.manage().timeouts().implicitlyWait(10, null);
+		Assert.assertEquals(driver.getTitle(),
+				"Online hopping Site for Mobiles, Electronics, Furniture, Grocery, Lifestyle, Books & More. Best Offers!");
+		
+	}
+	
+
+}
---
+++ test/test/src/test/java/Com/Sample/testcases/SellerTest.java
@@ -0,0 +1,45 @@
+package Com.Sample.testcases;
+
+import java.io.IOException;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.JavascriptExecutor;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import Com.Sample.base.basepage;
+import Com.Sample.pages.LoginPage;
+import Com.Sample.pages.SellerPage;
+
+public class SellerTest extends basepage {
+	SellerPage lp;
+	private static Logger logger = LogManager.getLogger(SellerTest.class);
+	public SellerTest() {
+		super();
+	}
+
+	@BeforeMethod
+	public void Setup() throws IOException {
+		setup();
+		lp = new SellerPage(null);
+	}
+
+	@Test(priority = 17, groups= {"sanity"})
+	public void SellerTest1() throws InterruptedException {
+		driver.findElement(LoginPage.btn_Login1).click();
+		Thread.sleep(2000);
+		driver.findElement(SellerPage.btn_Seller).click();
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("window.scrollBy(0,500)");
+		Thread.sleep(2000);
+		js.executeScript("window.scrollBy(0,1200)");
+		Thread.sleep(2000);
+		js.executeScript("window.scrollBy(0,-1700)");
+		Thread.sleep(2000);
+		Assert.assertEquals(driver.getTitle(),
+				"Sell Online on Flipkart | Grow your business with the leader in Indian e-commerce");
+		System.out.println("Assertion is passed");
+	}
+
+}
