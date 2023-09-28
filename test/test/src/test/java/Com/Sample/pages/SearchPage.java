6067be51ad0ae32c8c81d814e7e397a852c03d19
Gagan Kaushik <gagan.kaushik@nagarro.com>
2023-09-28 11:38:35 +0530

    Added Mavenized code with Selenium tests and updated pom.xml

--- test/test/src/test/java/Com/Sample/pages/SearchPage.java
+++ test/test/src/test/java/Com/Sample/pages/SearchPage.java
@@ -9,7 +9,7 @@ public class SearchPage {
 	public SearchPage(WebDriver driver) {
 		this.driver = driver;
 	}
-	public static By txt_search = By.xpath("//*[@id=\"container\"]/div/div[1]/div[1]/div[2]/div[2]/form/div/div/input");
+	public static By txt_search = By.xpath("//input[@placeholder='Search for Products, Brands and More']");
 	public static By btn_submit = By.xpath("//*[@id=\"container\"]/div/div[1]/div[1]/div[2]/div[2]/form/div/button");
 	public static By txt_search1 = By.xpath("//*[@id=\"container\"]/div/div[3]/div[1]/div[2]/div[6]/div/div/div/a/div[2]/div[1]/div[1]");
 	public static By btn_cart = By.xpath("//*[@id=\"container\"]/div/div[3]/div[1]/div[1]/div[2]/div/ul/li[1]/button");
